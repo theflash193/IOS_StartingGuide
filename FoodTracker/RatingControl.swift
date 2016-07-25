6ed23f4b65c5cbef8133ca38341aa8c25a3afc21
Gordon RASS-KWASI <grass-kw@e2r8p4.42.fr>
2016-07-25 19:29:47 +0200

    selectionner d'image fonctionnel

---
+++ FoodTracker/RatingControl.swift
@@ -0,0 +1,17 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by Gordon RASS-KWASI on 7/25/16.
+//  Copyright © 2016 Gordon RASS-KWASI. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIView {
+    // MARK: init
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+
+}
--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate {
+class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     // MARK: properties
     @IBOutlet weak var mealNameLabel: UILabel!
     @IBOutlet weak var naemTextField: UITextField!
@@ -25,11 +25,6 @@ class ViewController: UIViewController, UITextFieldDelegate {
         // Dispose of any resources that can be recreated.
     }
     
-    // MARK: actions
-    @IBAction func setDefaultLabelText(sender: UIButton) {
-        mealNameLabel.text = "Default Text"
-    }
-    
     // MARK: UITextFieldDelegate
     func textFieldShouldReturn(textField: UITextField) -> Bool {
         
@@ -43,5 +38,30 @@ class ViewController: UIViewController, UITextFieldDelegate {
     func textFieldDidEndEditing(textField: UITextField) {
         mealNameLabel.text = textField.text
     }
+    
+    // MARK: UIImagePickerControllerDelegate
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        let selectedImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+        photoimageView.image = selectedImage
+        dismissViewControllerAnimated(true, completion: nil)
+        
+    }
+    // MARK: actions
+    @IBAction func setDefaultLabelText(sender: UIButton) {
+        mealNameLabel.text = "Default Text"
+    }
+    
+    @IBAction func selectImageFromLibrary(sender: UITapGestureRecognizer) {
+        naemTextField.resignFirstResponder()
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.sourceType = .PhotoLibrary
+        imagePickerController.delegate = self
+        presentViewController(imagePickerController, animated: true, completion: nil)
+        
+    }
+
 }
 
