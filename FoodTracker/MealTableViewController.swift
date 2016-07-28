9aa52a44946eac8c24f86d66ac05db60c070be42
Gordon RASS-KWASI <grass-kw@e3r2p8.42.fr>
2016-07-28 14:09:14 +0200

    mode edition suppression repas

--- FoodTracker/MealTableViewController.swift
+++ FoodTracker/MealTableViewController.swift
@@ -15,9 +15,13 @@ class MealTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+         // Use the edit button item provided by the table view controller.
+        navigationItem.leftBarButtonItem = editButtonItem()
         
         // Load sample data
         loadSampleMeals()
+
 
     }
     
@@ -99,25 +103,26 @@ class MealTableViewController: UITableViewController {
             print("Adding new meal")
         }
     }
-    /*
+    
     // Override to support conditional editing of the table view.
     override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
+    
 
-    /*
+    
     // Override to support editing the table view.
     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
         if editingStyle == .Delete {
+            meals.removeAtIndex(indexPath.row)
             // Delete the row from the data source
             tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
         } else if editingStyle == .Insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
+    
 
     /*
     // Override to support rearranging the table view.
